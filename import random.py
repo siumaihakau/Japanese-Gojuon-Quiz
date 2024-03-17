a7437984da818466be50a0dce33728b07417ead9
TerrenceKHT <115112534+TerrenceKHT@users.noreply.github.com>
2024-03-17 20:42:34 +0000

    Add files via upload
    bug fixes

---
+++ import random.py
@@ -0,0 +1,91 @@
+import random
+gojuonLeft = 0
+# A dictionary that maps each Japanese character to its English transliteration
+transliterations = {
+    "あ": "a",
+    "い": "i",
+    "う": "u",
+    "え": "e",
+    "お": "o",
+    "か": "ka",
+    "き": "ki",
+    "く": "ku",
+    "け": "ke",
+    "こ": "ko",
+    "さ": "sa",
+    "し": "shi",
+    "す": "su",
+    "せ": "se",
+    "そ": "so",
+    "た": "ta",
+    "ち": "chi",
+    "つ": "tsu",
+    "て": "te",
+    "と": "to",
+    "な": "na",
+    "に": "ni",
+    "ぬ": "nu",
+    "ね": "ne",
+    "の": "no",
+    "は": "ha",
+    "ひ": "hi",
+    "ふ": "fu",
+    "へ": "he",
+    "ほ": "ho",
+    "ま": "ma",
+    "み": "mi",
+    "む": "mu",
+    "め": "me",
+    "も": "mo",
+    "や": "ya",
+    "ゆ": "yu",
+    "よ": "yo",
+    "ら": "ra",
+    "り": "ri",
+    "る": "ru",
+    "れ": "re",
+    "ろ": "ro",
+    "わ": "wa",
+    "を": "wo",
+    "ん": "n",
+    "が": "ga",
+    "ぎ": "gi",
+    "ぐ": "gu",
+    "げ": "ge",
+    "ご": "go",
+    "ざ": "za",
+    "じ": "ji",
+    "ず": "zu",
+    "ぜ": "ze",
+    "ぞ": "zo",
+    "だ": "da",
+    "ぢ": "ji",
+    "づ": "zu",
+    "で": "de",
+    "ど": "do",
+    "ば": "ba",
+    "び": "bi",
+    "ぶ": "bu",
+    "べ": "be",
+    "ぼ": "bo",
+    "ぱ": "pa",
+    "ぴ": "pi",
+    "ぷ": "pu",
+    "ぺ": "pe",
+    "ぽ": "po"
+}
+# Shuffle and print the characters for the user to guess
+gojuon = list(transliterations.keys())
+
+while gojuonLeft != 0:
+    gojuonLeft = len(gojuon)
+    random.shuffle(gojuon)
+    print(gojuon[0])
+    user_input = input("Enter the transliteration of the character: ")
+    if user_input == transliterations[gojuon[0]]:
+        print("Correct!")
+        gojuon.remove(gojuon[0])
+    elif user_input == ("?"):
+        print("There are", gojuonLeft, "questions left")
+    else:
+        print("Incorrect. The correct transliteration is: ", transliterations[gojuon[0]])
